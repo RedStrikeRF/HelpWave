2d09f14f7b68a903917015afb63e244d72b87d94
Илья Шубин <shubin152@gmail.com>
2025-04-03 18:44:37 +0500

    frontend initial commit

---
+++ frontend/src/app/app/App.tsx
@@ -0,0 +1,11 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      Начало работы над HelpWave
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/vite.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import checker from 'vite-plugin-checker'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    checker({ typescript: true })
+  ],
+  resolve: {
+    alias: [
+      { find: '@app', replacement: path.resolve(__dirname, 'src/app') },
+      { find: '@widgets', replacement: path.resolve(__dirname, 'src/widgets') },
+      { find: '@pages', replacement: path.resolve(__dirname, 'src/pages') },
+      { find: '@features', replacement: path.resolve(__dirname, 'src/features') },
+      { find: '@entities', replacement: path.resolve(__dirname, 'src/entities') },
+      { find: '@shared', replacement: path.resolve(__dirname, 'src/shared') }
+    ]
+  },
+  server: {
+    open: false,
+    port: 3000,
+    strictPort: true
+  },
+  preview: {
+    port: 3000 | 8000,
+    host: true
+  }
+})
