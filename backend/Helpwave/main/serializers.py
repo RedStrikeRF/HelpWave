dc9a470491d2de7a4d76119c7321a6beaae141b6
CerafimD <danilcerafim@gmail.com>
2025-04-20 18:03:01 +0500

    Added CRUD operations to all enteties, added JWT authorization

---
+++ backend/Helpwave/main/serializers.py
@@ -0,0 +1,106 @@
+from rest_framework import serializers, viewsets, permissions
+from django.contrib.auth import get_user_model
+from .models import Volunteer, Organizer, Meeting
+
+User = get_user_model()
+
+# Serializers
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'password')
+        extra_kwargs = {
+            'password': {'write_only': True},
+            'username': {'validators': []}  # Disable uniqueness validator for updates
+        }
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop('password', None)
+        if password:
+            instance.set_password(password)
+        return super().update(instance, validated_data)
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = User(**validated_data)
+        user.set_password(password)  # Хеширование пароля
+        user.save()
+        return user
+
+class VolunteerSerializer(serializers.ModelSerializer):
+    user = UserSerializer()  # Вложенный сериализатор для User
+
+    class Meta:
+        model = Volunteer
+        fields = ('id', 'user', 'bio', 'skills', 'availability')
+
+    def create(self, validated_data):
+        user_data = validated_data.pop('user')
+        user_serializer = UserSerializer(data=user_data)
+        if user_serializer.is_valid():
+            user = user_serializer.save()
+            volunteer = Volunteer.objects.create(user=user, **validated_data)
+            return volunteer
+        raise serializers.ValidationError(user_serializer.errors)
+
+
+class OrganizerSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+
+    class Meta:
+        model = Organizer
+        fields = ('id', 'user', 'description', 'contact_information')
+
+    def create(self, validated_data):
+        user_data = validated_data.pop('user')
+        user_serializer = UserSerializer(data=user_data)
+        if user_serializer.is_valid():
+            user = user_serializer.save()
+            organizer = Organizer.objects.create(user=user, **validated_data)
+            return organizer
+        raise serializers.ValidationError(user_serializer.errors)
+
+    def update(self, instance, validated_data):
+        user_data = validated_data.pop('user', {})
+
+        # Get the current user instance
+        user_instance = instance.user
+
+        # Handle password separately if provided
+        password = user_data.pop('password', None)
+
+        # Update user fields
+        user_serializer = UserSerializer(
+            instance=user_instance,
+            data=user_data,
+            partial=True  # Crucial for partial updates
+        )
+
+        if user_serializer.is_valid():
+            if password:
+                user_instance.set_password(password)
+            user_serializer.save()
+        else:
+            raise serializers.ValidationError({
+                'user': user_serializer.errors
+            })
+
+        # Update organizer fields
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+
+        return instance
+
+class MeetingSerializer(serializers.ModelSerializer):
+    organizer = OrganizerSerializer(read_only=True) #Сериализует данные органайзера
+
+    class Meta:
+        model = Meeting
+        fields = '__all__'
+        read_only_fields = ('organizer',)
+
+# ViewSets
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
---
+++ backend/Helpwave/main/urls.py
@@ -0,0 +1,34 @@
+# main/urls.py
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+from .views import (
+    UserViewSet,
+    VolunteerViewSet,
+    OrganizerViewSet,
+    MeetingViewSet,
+    ObtainTokenPairWithVolunteerOrOrganizerView
+)
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'volunteers', VolunteerViewSet)
+router.register(r'organizers', OrganizerViewSet)
+router.register(r'meetings', MeetingViewSet)
+
+
+urlpatterns = [
+    # Маршрут для получения токена (JWT)
+    path('token/', ObtainTokenPairWithVolunteerOrOrganizerView.as_view(), name='token_obtain_pair'),
+
+    # Маршрут для обновления токена (JWT)
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    # Маршрут для верификации токена (JWT)
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('', include(router.urls)),
+]
